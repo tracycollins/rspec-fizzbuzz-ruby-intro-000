3a5f9f5e535c785c9dd8a3ef6fca0669c4156e08
Tracy Collins <tc@threeceemedia.com>
2020-02-18 00:43:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     "Fizzbuzz"
-  end
-  if int % 3 == 0
+  elsif int % 3 == 0
     "Fizz"
-  end
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
   end
 end
