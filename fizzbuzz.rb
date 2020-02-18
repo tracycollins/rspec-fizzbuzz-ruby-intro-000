168edffd7470d7a0089b0c38a09af163996fae20
Tracy Collins <tc@threeceemedia.com>
2020-02-18 00:42:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    "Fizzbuzz"
+  end
   if int % 3 == 0
     "Fizz"
+  end
+  if int % 5 == 0
+    "Buzz"
   end
 end
